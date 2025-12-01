8b85ff5b247dfec0bc14aabc3a689eb5072f96ac
bobhroy <bobhroy@gmail.com>
2025-11-30 19:42:12 -0800

    Implement Get authorized user information

--- user-service/src/main/java/com/bobgarage/userservice/controllers/AuthController.java
+++ user-service/src/main/java/com/bobgarage/userservice/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.bobgarage.userservice.controllers;
 
 import com.bobgarage.userservice.dtos.JwtResponse;
 import com.bobgarage.userservice.dtos.LoginRequest;
+import com.bobgarage.userservice.dtos.UserDto;
+import com.bobgarage.userservice.mappers.UserMapper;
+import com.bobgarage.userservice.repositories.UserRepository;
 import com.bobgarage.userservice.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -43,5 +49,20 @@ public class AuthController {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Void> handleBadCredentialsException() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 }
